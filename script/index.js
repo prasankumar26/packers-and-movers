50b154ceb237b9c8bbb7b475f7fea5b1500718ba
unknown <viratprasan14@gmail.com>
2020-09-14 19:11:14 +0530

    added responsiveness and scroll top btn

--- script/index.js
+++ script/index.js
@@ -143,11 +143,25 @@
 
 
 
+// scroll on top 
+
+$(document).ready(function(){ 
+  $(window).scroll(function(){ 
+      if ($(this).scrollTop() > 100) { 
+          $('#scroll').fadeIn(); 
+      } else { 
+          $('#scroll').fadeOut(); 
+      } 
+  }); 
+  $('#scroll').click(function(){ 
+      $("html, body").animate({ scrollTop: 0 }, 600); 
+      return false; 
+  }); 
+});
 
 
 
-
-
+//increment
 function incrementValue()
 {
     var value = parseInt(document.getElementById('number').value, 10);
